6185ecfc68e47ac9c604f42286ba6bff59afbdd3
Yawar Abass <bhatsharik6146@gmail.com>
2022-11-09 20:43:47 +0530

    added card.css

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ function toggleMenu() {
     toggleMenu.classList.toggle('active');
     navigation.classList.toggle('active');
 }
+
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
