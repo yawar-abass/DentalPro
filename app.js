9808b897c6b3eb865b1115ebb7e07af7aa2b2dc0
Yawar Abass <bhatsharik6146@gmail.com>
2022-11-10 14:51:05 +0530

    Added footer and about section

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 
-function toggleMenu() {
-    const toggleMenu = document.querySelector('.toggleMenu');
-    const navigation = document.querySelector('.navigation');
-    toggleMenu.classList.toggle('active');
-    navigation.classList.toggle('active');
+// function toggleMenu() {
+//   const toggleMenu = document.querySelector('.toggleMenu');
+//   const cross = document.querySelector('.cross');
+//   const navigation = document.querySelector('.navigation');
+//   toggleMenu.classList.toggle('active');
+//   navigation.classList.toggle('active');
+
+// }
+function toggleMenu(x) {
+  const navigation = document.querySelector('.navigation');
+  x.classList.toggle("change");
+  navigation.classList.toggle('active');
 }
 
 var swiper = new Swiper('.blog-slider', {
-    spaceBetween: 30,
-    effect: 'fade',
-    loop: true,
-    mousewheel: {
-      invert: false,
-    },
-    // autoHeight: true,
-    pagination: {
-      el: '.blog-slider__pagination',
-      clickable: true,
-    }
-  });
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
